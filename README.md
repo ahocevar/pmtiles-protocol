ce093d80ad91912fcb6366b24981633c1c55d534
Andreas Hocevar <andreas.hocevar@gmail.com>
2024-10-21 19:02:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# pmtiles-protocol
+
+Provides `fetch` and `XMLHttpRequest` versions that support urls starting with `pmtiles://`, returning the respective TileJSON or tiles. It is meant to be used in browser applications.
+
+## Supported URLs
+
+### TileJSON
+
+- `pmtiles://https://example.com/path/to/mytiles.pmtiles` (absolute)
+- `pmtiles://path/to/mytiles.pmtiles` (relative to `window.location.href`)
+
+### Tiles
+
+- `pmtiles://https://example.com/path/to/mytiles.pmtiles/{z}/{x}/{y}.mvt` (absolute)
+- `pmtiles://path/to/mytiles.pmtiles/{z}/{x}/{y}.mvt` (relative to `window.location.href`)
+
+## How to use
+
+`pmtiles-protocol` can be through a dedicated `fetch()` function or a dedicated `XMLHttpRequest` replacement:
+
+```js
+import { fetch, XMLHttpRequest } from 'pmtiles-protocol';
+```
+
+`fetch()` and `XMLHttpRequest` are now available in the module scope. If the module scope is not enough, global overrides for `fetch()` and `XMLHttpRequest` are also available:
+
+```js
+import { register } from 'pmtiles-protocol';
+
+const unregister = register();
+```
+
+To restore the original global `fetch()` and `XMLHttpRequest` versions, call
+
+```js
+unregister();
+```
+
+## Examples
+
+### fetch
+
+```js
+fetch('pmtiles://https://example.com/mytiles.pmtiles/0/0/0.mvt');
+```
+
+fetches the 0/0/0 tile from the PMTiles file at `https://example.com/mytiles.pmtiles`.
+
+### XMLHttpRequest
+
+```js
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'pmtiles://path/to/mytiles.pmtiles');
+xhr.onload = () => {
+  console.log('TileJSON', xhr.responseText);
+};
+```
+
+logs the TileJSON from the PMTiles file at `path/to/mytiles.pmtiles` (relative to `window.location.href`) to the console.
+
+### In a Mapbox Style document
+
+The `pmtiles` source below will use the TileJSON and tiles from the relative url `path/to/mytiles.pmtiles`:
+
+```json
+{
+  "sources": {
+    "pmtiles": {
+      "type": "vector",
+      "url": "pmtiles://path/to/mytiles.pmtiles"
+    }
+  }
+}
+```
+
+## Limitations
+
+This package won't add support for PMTiles when loading images by simply setting the `src` of an `Image` or `HTTPImageElement`. However, using an object url, the following would work:
+
+```js
+const img = new Image();
+fetch('pmtiles://path/to/mytiles.pmtiles/0/0/0.png')
+  .then((response) => response.blob())
+  .then((blob) => {
+    const objectUrl = URL.createObjectURL(blob);
+    img.onload = () => {
+      console.log('Image loaded');
+      URL.revokeObjectURL(objectUrl);
+    };
+    img.onerror = () => {
+      console.error('Image load error');
+      URL.revokeObjectURL(objectUrl);
+    };
+    img.src = objectUrl;
+  })
+  .catch((error) => {
+    console.error('Fetch error:', error);
+  });
+```
+
+### fetch()
+
+No known limitations.
+
+### XMLHttpRequest
+
+The limitations below only apply when `XMLHttpRequest` is used with a `pmtiles://` url.
+
+- Only the `load` and `error` events are fired.
+- The only methods that act on the PMTiles file are `open()` and `send()`.
+- Only the `response` and `responseText` properties are supported.
+- Only the `200` and `404` status codes are used.
---
+++ index.js
@@ -0,0 +1,194 @@
+import { PMTiles } from 'pmtiles';
+
+const contentType = [
+  'application/octet-stream',
+  'application/x-protobuf',
+  'image/png',
+  'image/jpeg',
+  'image/webp',
+  'image/avif',
+];
+
+const tileUrlRegex = /\/([0-9]+)\/([0-9]+)\/([0-9]+).(mvt|png|jpg|webp|avif)$/;
+const protocolRegex = /^pmtiles:\/\//;
+const fixUrlRegex = /^http(s?)\/\//; // workaround for broken URLs in Safari
+/** @type {Object<string, PMTiles>} */
+const pmtilesByUrl = {};
+
+const { fetch: originalFetch, XMLHttpRequest: OriginalXHR } = window;
+
+export const fetch = new Proxy(originalFetch, {
+  apply: async (target, that, [input, init]) => {
+    /** @type {string} */
+    let url;
+    if (input instanceof Request) {
+      if (input.method !== 'GET') {
+        return target.call(that, input, init);
+      }
+      url = input.url;
+    } else {
+      url = input.toString();
+    }
+    if (!protocolRegex.test(url)) {
+      return target.call(that, input, init);
+    }
+    url = url.replace(protocolRegex, '').replace(fixUrlRegex, 'http$1://');
+    let baseUrl = url;
+    /** @type {[number, number, number]|null} */
+    let zxy = null;
+    if (tileUrlRegex.test(url)) {
+      baseUrl = url.replace(tileUrlRegex, '');
+      zxy = /** @type {[number, number, number]} */ (
+        url.match(tileUrlRegex)?.slice(1).map(Number)
+      );
+    }
+    if (!(baseUrl in pmtilesByUrl)) {
+      pmtilesByUrl[baseUrl] = new PMTiles(baseUrl);
+    }
+    if (zxy) {
+      const tileResult = await pmtilesByUrl[baseUrl].getZxy(...zxy);
+      if (!tileResult) {
+        return new Response(null, { status: 404 });
+      }
+      const header = await pmtilesByUrl[baseUrl].getHeader();
+      return new Response(tileResult.data, {
+        headers: { 'Content-Type': contentType[header.tileType] },
+      });
+    } else {
+      const data = await pmtilesByUrl[baseUrl].getTileJson(
+        'pmtiles://' + baseUrl,
+      );
+      return new Response(JSON.stringify(data), {
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+  },
+});
+
+export class XMLHttpRequest extends OriginalXHR {
+  constructor() {
+    super();
+    /** @type {string} */
+    let baseUrl;
+    /** @type {undefined|((event: ProgressEvent) => void)} */
+    let onload;
+    /** @type {undefined|((event: ProgressEvent) => void)} */
+    let onerror;
+    /** @type {ArrayBuffer|null} */
+    let response;
+    /** @type {string|null} */
+    let responseText,
+      /** @type {200 | 404} */
+      status;
+    /** @type {Array<number>|null} */
+    let zxy = null;
+    return new Proxy(this, {
+      get: (target, prop) => {
+        if (prop === 'open') {
+          /**
+           * @param {string} method
+           * @param {string} url
+           * @param {boolean} async
+           * @param {string} user
+           * @param {string} password
+           */
+          return (method, url, async = true, user, password) => {
+            if (method !== 'GET' || !protocolRegex.test(url)) {
+              return this.open(method, url, async, user, password);
+            }
+            url = url
+              .replace(protocolRegex, '')
+              .replace(fixUrlRegex, 'http$1://');
+            baseUrl = url;
+            /** @type {[number, number, number]} */
+            if (tileUrlRegex.test(url)) {
+              baseUrl = url.replace(tileUrlRegex, '');
+              zxy = url.match(tileUrlRegex)?.slice(1).map(Number) || null;
+            }
+            if (!(baseUrl in pmtilesByUrl)) {
+              pmtilesByUrl[baseUrl] = new PMTiles(baseUrl);
+            }
+          };
+        } else if (prop === 'send') {
+          /**
+           * @param {XMLHttpRequestBodyInit|Document|null} [body]
+           */
+          return async (body) => {
+            if (!baseUrl) {
+              return this.send(body);
+            }
+            try {
+              const loadEvent = new ProgressEvent('load');
+              if (zxy) {
+                const tileResult = await pmtilesByUrl[baseUrl].getZxy(
+                  zxy[0],
+                  zxy[1],
+                  zxy[2],
+                );
+                if (tileResult) {
+                  status = 200;
+                  response = tileResult.data;
+                } else {
+                  status = 404;
+                  response = null;
+                }
+                this.dispatchEvent(loadEvent);
+                if (onload) {
+                  onload(loadEvent);
+                }
+              } else {
+                const tileResult = await pmtilesByUrl[baseUrl].getTileJson(
+                  'pmtiles://' + baseUrl,
+                );
+                status = 200;
+                responseText = JSON.stringify(tileResult);
+                this.dispatchEvent(loadEvent);
+                if (onload) {
+                  onload(loadEvent);
+                }
+              }
+            } catch (e) {
+              console.error(e);
+              const errorEvent = new ProgressEvent('error');
+              this.dispatchEvent(errorEvent);
+              if (onerror) {
+                onerror(errorEvent);
+              }
+            }
+          };
+        } else if (prop === 'response' && response) {
+          return response;
+        } else if (prop === 'responseText' && responseText) {
+          return responseText;
+        } else if (prop === 'status' && status) {
+          return status;
+        }
+        // @ts-expect-error
+        return target[prop];
+      },
+      set: (target, prop, value) => {
+        if (prop === 'onload') {
+          onload = value;
+        } else if (prop === 'onerror') {
+          onerror = value;
+        }
+        // @ts-expect-error
+        target[prop] = value;
+        return true;
+      },
+    });
+  }
+}
+
+/**
+ * Registers fetch and XMLHttpRequest global overrides.
+ * @returns {() => void} Unregister function
+ */
+export const register = () => {
+  window.fetch = fetch;
+  window.XMLHttpRequest = XMLHttpRequest;
+  return () => {
+    window.fetch = originalFetch;
+    window.XMLHttpRequest = OriginalXHR;
+  };
+};
